f00b07888d16c5fbf0a8b578474768f12521e619
kritishasojitra <kirtisojitra436@gmail.com>
2024-06-06 11:12:16 +0530

    Singup-Login-Form

---
+++ singuplogin/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import Singuplogin from './componet/Singuplogin';
+
+function App() {
+  return (
+    <div className="App">
+     <Singuplogin/>
+    </div>
+  );
+}
+
+export default App;
